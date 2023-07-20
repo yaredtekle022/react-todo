53696349f8eccfd8f7f425e6f71787a63db6249b
yaredtt <yaredtekle22@gmail.com>
2023-07-20 10:44:02 +0300

    create todo app with react

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,5 +1,5 @@
-import Header from './Header';
-import TodosLogic from './TodosLogic';
+import Header from './Header.js';
+import TodosLogic from './TodosLogic.js';
 
 const TodoApp = () => (
   <>
---
+++ src/components/TodosLogic.js
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import InputTodo from './InputTodo.js';
+import TodosList from './TodoList.js';
+
+const TodosLogic = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: uuidv4(),
+      title: 'Setup development environment',
+      completed: true,
+    },
+    {
+      id: uuidv4(),
+      title: 'Develop website and add content',
+      completed: false,
+    },
+    {
+      id: uuidv4(),
+      title: 'Deploy to live server',
+      completed: false,
+    },
+  ]);
+
+  const delTodo = (id) => {
+    setTodos([
+      ...todos.filter((todo) => todo.id !== id),
+    ]);
+  };
+
+  const addTodoItem = (title) => {
+    const newTodo = {
+      id: uuidv4(),
+      title,
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
+  };
+
+  return (
+    <div>
+      <InputTodo addTodoItem={addTodoItem} />
+      <TodosList todosProps={todos} setTodos={setTodos} delTodo={delTodo} />
+    </div>
+  );
+};
+export default TodosLogic;
